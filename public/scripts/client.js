898448bd9ea3039e37c3fb9f801c00f44942190d
Cecil Vogrinetz <cvogrinetz@gmail.com>
2021-05-24 02:59:38 +0000

    Add quick eslint for cleaner code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,54 +3,55 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready( (event) => {
+$(document).ready((event) => {
 
 
   // Will render each seperate tweet to page.
-const renderTweets = function(tweets) {
-  for (const element of tweets) {
-    let tweets = createTweetElement(element);
-    $('.tweetSection').prepend(tweets)
-  }
-};
+  const renderTweets = function(tweets) {
+    for (const element of tweets) {
+      let tweets = createTweetElement(element);
+      $('.tweetSection').prepend(tweets);
+    }
+  };
 
-// Escape function that stops cross-site script
-const escape = function (str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
+  // Escape function that stops cross-site script
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
 
-// Scroll to top button functionality
-$(window).scroll(function() {
-  var height = $(window).scrollTop();
-  if (height > 100) {
+  // Scroll to top button functionality
+  $(window).scroll(function() {
+    let height = $(window).scrollTop();
+    if (height > 100) {
       $('#scrollToTop').fadeIn();
-  } else {
+    } else {
       $('#scrollToTop').fadeOut();
-  }
-});
-$(document).ready(function() {
-  $("#scrollToTop").click(function(event) {
+    }
+  });
+
+  $(document).ready(function() {
+    $("#scrollToTop").click(function(event) {
       event.preventDefault();
       $("html, body").animate({ scrollTop: 0 }, "slow");
       return false;
+    });
   });
 
-});
 
 
-// Ajax GET request to render tweets right when page loads
-$.ajax('/tweets', {method: 'GET'})
-      .then(function (tweets) {
+  // Ajax GET request to render tweets right when page loads
+  $.ajax('/tweets', {method: 'GET'})
+    .then(function(tweets) {
       renderTweets(tweets);
     });
 
 
-// Creates new tweet structure from incoming data.
-const createTweetElement = (tweetObject) => {
-  const $tweet = 
+  // Creates new tweet structure from incoming data.
+  const createTweetElement = (tweetObject) => {
+    const $tweet =
 
  `<article class="tweetContainer">
   <header class="tweetHeader">
@@ -83,68 +84,68 @@ const createTweetElement = (tweetObject) => {
   </footer>
   </article>`;
 
-  return $tweet;
+    return $tweet;
   
-};
+  };
 
       
-// Ajax Post and Get request to grab tweet info and then render it to page AFTER new tweet created.  
-const loadTweets = () => {
+  // Ajax Post and Get request to grab tweet info and then render it to page AFTER new tweet created.
+  const loadTweets = () => {
 
 
 
-  $('#form').submit(function (event) {
+    $('#form').submit(function(event) {
 
-    let data = {
-      text: $('#tweet-text').val()
-    };
+      let data = {
+        text: $('#tweet-text').val()
+      };
 
 
-    // If there is NO input this error message will prompt user
-    if (!data.text.length) {
+      // If there is NO input this error message will prompt user
+      if (!data.text.length) {
 
-      $('.error').remove();
-      $('#new-tweet').removeClass('error')
+        $('.error').remove();
+        $('#new-tweet').removeClass('error');
       
-      $('#new-tweet').addClass('error')
-      $('#error').after(`<span class="error"><i class="fas fa-cat"></i>Cat got your tongue? Dont't be shy, try again!<i class="fas fa-cat"></i></span>`)
-      event.preventDefault();
-    }
-    // If the maximum character limit is exceeded then this error message will prompt user  
-    if (data.text.length > 140) {
+        $('#new-tweet').addClass('error');
+        $('#error').after(`<span class="error"><i class="fas fa-cat"></i>Cat got your tongue? Dont't be shy, try again!<i class="fas fa-cat"></i></span>`);
+        event.preventDefault();
+      }
+      // If the maximum character limit is exceeded then this error message will prompt user
+      if (data.text.length > 140) {
       
-      $('.error').remove();
-      $('#new-tweet').removeClass('error')
+        $('.error').remove();
+        $('#new-tweet').removeClass('error');
 
-      $('#new-tweet').addClass('error')
-      $('#error').after(`<span class="error"><i class="fas fa-bomb"></i>System will overload if you don't settle down. Try again but with smaller words!<i class="fas fa-bomb"></i></span>`)
-      event.preventDefault();
-    }
+        $('#new-tweet').addClass('error');
+        $('#error').after(`<span class="error"><i class="fas fa-bomb"></i>System will overload if you don't settle down. Try again but with smaller words!<i class="fas fa-bomb"></i></span>`);
+        event.preventDefault();
+      }
 
-    // If all is good with the input form then this will run and render the tweets proper
-    if (data.text.length && data.text.length <= 140) {
-      $('.error').remove();
-      $('#new-tweet').removeClass('error')
-      event.preventDefault();
+      // If all is good with the input form then this will run and render the tweets proper
+      if (data.text.length && data.text.length <= 140) {
+        $('.error').remove();
+        $('#new-tweet').removeClass('error');
+        event.preventDefault();
      
-    let message = $(this).serialize();
-    $.ajax({
-      type: "POST",
-      url: "/tweets",
-      data: message,
-      })
-      .done((event) => {
-        $.ajax('/tweets', {method: 'GET'})
-        .then(function (tweets) {
-          $('.tweetSection').prepend(createTweetElement(tweets[tweets.length - 1]));
-          $('#counter').text(140);
-          $('#form')[0].reset();
-        });
-      });
-    };
-  }); 
-}
+        let message = $(this).serialize();
+        $.ajax({
+          type: "POST",
+          url: "/tweets",
+          data: message,
+        })
+          .done((event) => {
+            $.ajax('/tweets', {method: 'GET'})
+              .then(function(tweets) {
+                $('.tweetSection').prepend(createTweetElement(tweets[tweets.length - 1]));
+                $('#counter').text(140);
+                $('#form')[0].reset();
+              });
+          });
+      }
+    });
+  };
 
-loadTweets();
+  loadTweets();
 
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,17 @@
-$(document).ready( (event) => {
+$(document).ready((event) => {
 
   
   let  text = $('#tweet-text').on("keyup", event => {
     let maxLength = 140;
     let length = $(text).val().length;
-    length = maxLength-length;
+    length = maxLength - length;
     $(this.counter).text(length);
     
     let counter = $(this.counter);
-    if(length < 0) {
-      counter.css("color", "#ff0000") 
+    if (length < 0) {
+      counter.css("color", "#ff0000");
     } else {
-      counter.css("color", "#545149")
+      counter.css("color", "#545149");
     }
   });
   
